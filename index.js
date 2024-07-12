e57321ddbcbc5c90f70da75151b0cfc7f3c89ffa
reffcp_ultratech <crefugio-ext@ultratech-inc.com>
2024-07-12 02:26:56 -0600

    feat: :heavy_plus_sign: se agrega ajsute para uso de variables de entorno

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -7,10 +8,10 @@ app.use(cors());
 const routes = require("./src/router");
 routes(app);
 
-const port = 3001;
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(
+    `Servidor escuchando en http://${process.env.HOST}:${process.env.PORT}`
+  );
 });
 
 module.exports = app;
