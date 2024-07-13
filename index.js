ec8bbdfecf195ef0e3c9777c3cf8116032593195
reffcp_ultratech <crefugio-ext@ultratech-inc.com>
2024-07-12 23:10:54 -0600

    build: :rocket: ajuste para manejar https

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+require("./config.js");
+const routes = require("./src/router");
+const https = require("https");
 
 const app = express();
 app.use(cors());
-
-require("./config.js");
-
-const routes = require("./src/router");
 routes(app);
 
-app.listen(process.env.PORT, process.env.HOST, () => {
-  console.log(
-    `Servidor escuchando en http://${process.env.HOST}:${process.env.PORT}`
-  );
-});
+if (process.env.ENVIROMENT === "development") {
+  app.listen(process.env.API_PORT, process.env.API_HOST, () => {
+    console.log(
+      `Server is running on http://localhost:${process.env.API_PORT}`
+    );
+  });
+} else {
+  const options = {
+    key: fs.readFileSync(process.env.WEB_SERVER_PRIVATE_KEY_PATH),
+    cert: fs.readFileSync(process.env.WEB_SERVER_FULLCHAIN_CERT_PATH),
+  };
+  https
+    .createServer(options, app)
+    .listen(process.env.API_PORT, process.env.API_HOST, () => {
+      console.log(
+        `Server is running on https://${process.env.API_HOST}:${process.env.API_PORT}`
+      );
+    });
+}
 
 module.exports = app;
